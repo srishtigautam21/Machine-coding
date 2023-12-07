d4876de3ac3ea87e53ef12ed5017f6cc738f6181
Srishti Gautam <82943455+srishtigautam21@users.noreply.github.com>
2023-12-07 12:59:59 +0530

    added description to some js functions

--- Date Picker/main.js
+++ Date Picker/main.js
@@ -36,11 +36,13 @@ selected_date_element.textContent = formatDate(date);
 selected_date_element.dataset.value = selectedDate;
 
 populateDays();
-
+//Event listeners
 datePicker.addEventListener("click", handletoggleEvent);
 leftArrow.addEventListener("click", handlePrevMonth);
 rightArrow.addEventListener("click", handleNextMonth);
 
+//Handles the opening and closing of date picker dropdown on the click of selected date div only then it
+// should close otherwise on click inside the date picker it shouldn't close
 function handletoggleEvent(e) {
   if (
     !checkEventPathForClass(e.target, [
@@ -77,6 +79,7 @@ function handlePrevMonth() {
   populateDays();
 }
 
+//func to handle the number of days in each month and the onclick of each date to update the selected date
 function populateDays() {
   days.innerHTML = "";
   let amount_of_days = 31;
@@ -88,8 +91,8 @@ function populateDays() {
   }
   for (let i = 0; i < amount_of_days; i++) {
     const day_element = document.createElement("div");
-    day_element.classList.add("date");
-    day_element.textContent = i + 1;
+    day_element.classList.add("date"); //date class is added to div
+    day_element.textContent = i + 1; //print each date from 1 to 30/31 on screen
 
     if (
       selectedDay === i + 1 &&
@@ -107,13 +110,12 @@ function populateDays() {
       selected_date_element.dataset.value = selectedDate;
       populateDays();
     });
-    days.appendChild(day_element);
+    days.appendChild(day_element); //to add this as child element of dates class
   }
 }
 
 //Helper func
 function checkEventPathForClass(path, selector) {
-  console.log(path, path.classList.value);
   if (selector.includes(path.classList.value)) {
     return true;
   } else {
