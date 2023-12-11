2348cfa47a1b2d3eeb145a7ad5409a1a37dccfb2
Srishti Gautam <82943455+srishtigautam21@users.noreply.github.com>
2023-12-11 15:15:26 +0530

    Add the unselectable functionality

--- Polls/index.js
+++ Polls/index.js
@@ -1,22 +1,28 @@
 const selectAll = document.querySelectorAll(".content");
 const progress = document.querySelectorAll(".progress");
 const percentage = document.querySelectorAll(".percntage");
-const selectedColumn = document.querySelector(".each-coloumn");
+const selectedColumn = document.querySelectorAll(".each-coloumn");
 
 const handlecolumnClick = () => {
   for (let i = 0; i < selectAll.length; i++) {
-    console.log(selectAll[i]);
     progress[i].style.display = "block";
     percentage[i].style.display = "block";
   }
 };
 
-const handleSelectedColoumn = () => {
-  selectedColumn.classList.add("active");
-  console.log(selectedColumn);
+const handleSelectedColoumn = (index) => {
+  selectedColumn[index].classList.add("active");
+  for (let i = 0; i < selectedColumn.length; i++) {
+    if (i === index) {
+      continue;
+    }
+    selectedColumn[i].classList.add("notActive");
+  }
 };
 
 for (let i = 0; i < selectAll.length; i++) {
   selectAll[i].addEventListener("click", () => handlecolumnClick());
 }
-selectedColumn.addEventListener("click", () => handleSelectedColoumn());
+for (let i = 0; i < selectedColumn.length; i++) {
+  selectedColumn[i].addEventListener("click", () => handleSelectedColoumn(i));
+}
